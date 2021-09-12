3d27544ee89ff9b5ab769610585b57d460a2d079
rodgpe <19214482+RodGpe@users.noreply.github.com>
2021-09-12 05:56:51 +0000

    se agregaron comentarios al dockerfile

--- node_docker_files/index.js
+++ node_docker_files/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("<h2>La solicitud fue procesada por un reverse proxy nginx y la pasó a un proceso de node.js corriendo Express</h2>");
+    res.send("<h2>La solicitud fue procesada por reverse proxy NGINX y la pasó a un proceso de Node.js corriendo Express</h2>");
 });
 
 const port = process.env.PORT || 3000;
